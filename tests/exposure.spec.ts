cfcc021b408509b1727662f4ee01dc86d387f36b
Michael J. Roberts <michael@observerly.com>
2024-04-13 15:53:53 +0100

    feat: add client.exposure.store() router handler.
    feat: add client.exposure.store() router handler. Includes associated test suite for module export definition and expected output from API route.

--- tests/exposure.spec.ts
+++ tests/exposure.spec.ts
@@ -55,6 +55,45 @@ suite('@observerly/hyper NOX API Observing Exposure Client', () => {
       if (!isDataResult(status)) return
       expect(status).toStrictEqual({})
     })
+
+    it('should be able to store an exposure from the camera', async () => {
+      const client = setupClient(getURL('/api/v1/'))
+      const store = await client.camera.storeExposure({
+        bucketName: 'test.ex.observerly.com',
+        duration: 130,
+        start: '2021-05-15T03:00:00.000Z',
+        filter: 'Hα',
+        isDark: false,
+        isFlat: false,
+        mimetype: 'application/fits',
+        target: 'M42',
+        userId: 'sK0EFowNCXQ0JOK4UES2AjSnrVc2',
+        uuid: '01H3WK0383PNKTT0240B53JX5N',
+        mjd: 59379.0,
+        equinox: 2021.5,
+        ra: 83.82208,
+        dec: -5.39111,
+        telescope: 'Namibiascope 1',
+        instrument: '20" AG Optical iDK Planewave L-500s',
+        observer: 'Michael Roberts'
+      })
+      expect(isDataResult(store)).toBe(true)
+      if (!isDataResult(store)) return
+      expect(store).toStrictEqual({
+        ccdXSize: 1463,
+        ccdYSize: 1168,
+        isDark: false,
+        isFlat: false,
+        locations: [
+          'gs://test.ex.observerly.com/sK0EFowNCXQ0JOK4UES2AjSnrVc2/01H3WK0383PNKTT0240B53JX5N/M42_[Hα]_monochrome_M_130s_2021-05-15T03:00:00.000Z.fits'
+        ],
+        maxADU: 65535,
+        mimetype: 'application/fits',
+        sensor: 'Monochrome',
+        userId: 'sK0EFowNCXQ0JOK4UES2AjSnrVc2',
+        uuid: '01H3WK0383PNKTT0240B53JX5N'
+      })
+    })
   })
 })
 
